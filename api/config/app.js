ec6599f91f99d59cbf19795f42fa528bc5ed30b0
Lucas Melo <contatolucasgmelo@gmail.com>
2023-03-23 16:28:02 -0300

    fix: api limit

--- api/config/app.js
+++ api/config/app.js
@@ -10,6 +10,7 @@ const dbConnection = require("./dbConnection");
 dbConnection();
 
 const app = express();
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
--- api/server.js
+++ api/server.js
@@ -1,5 +1,3 @@
 const app = require("./config/app");
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
 app.listen(3003);
