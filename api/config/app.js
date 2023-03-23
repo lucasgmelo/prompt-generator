364d49bd5391fa8ce5ebcd90d89a880bf4c8e3ab
Lucas Melo <contatolucasgmelo@gmail.com>
2023-03-23 09:47:53 -0300

    feat: update limit

--- api/config/app.js
+++ api/config/app.js
@@ -10,6 +10,8 @@ const dbConnection = require("./dbConnection");
 dbConnection();
 
 const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
 app.use(express.json());
 app.use(routes);
--- api/server.js
+++ api/server.js
@@ -1,3 +1,5 @@
 const app = require("./config/app");
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.listen(3003);
